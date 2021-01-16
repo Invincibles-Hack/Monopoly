9a9c2898de01645a8cffe08d22393f54383fd6db
hiteshzamad <69067774+hiteshzamad@users.noreply.github.com>
2021-01-16 19:37:53 +0530

    2nd Commit

---
+++ Assets/Script/ScriptGameScene.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptGameScene : MonoBehaviour{
+
+    public Sprite dice1, dice2, dice3, dice4, dice5, dice6;
+
+    private float timeDiceRoll;
+    private Transform transformDice;
+
+    void Start(){
+        transformDice = transform.Find("ButtonDice");
+        transformDice.GetComponent<Button>().onClick.AddListener(OnDiceClick);
+    }
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            ShowExitDialog();
+        }
+        if(timeDiceRoll > 0.0f){
+            RollDice();
+        }
+    }
+
+    void OnDiceClick(){
+        timeDiceRoll = 1.0f;
+    }
+
+    void OnDiceResult(int result){
+        Debug.Log(result);
+    }
+
+    void RollDice(){
+        timeDiceRoll -= Time.deltaTime;
+        int diceCount  = Random.Range(1, 6);
+        switch (diceCount){
+            case 1:
+                transformDice.GetComponent<Image>().sprite = dice1;
+                break;
+            case 2:
+                transformDice.GetComponent<Image>().sprite = dice2;
+                break;
+            case 3:
+                transformDice.GetComponent<Image>().sprite = dice3;
+                break;
+            case 4:
+                transformDice.GetComponent<Image>().sprite = dice4;
+                break;
+            case 5:
+                transformDice.GetComponent<Image>().sprite = dice5;
+                break;
+            case 6:
+                transformDice.GetComponent<Image>().sprite = dice6;
+                break;
+        }
+        if(timeDiceRoll < 0.0f){
+            OnDiceResult(diceCount);
+        }
+    }
+
+    void ShowExitDialog(){
+        Debug.Log("Exit Clicked");
+    }
+}
---
+++ Assets/Script/ScriptHomeScene.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScriptHomeScene : MonoBehaviour{
+
+    void Start(){
+        Button buttonPlay = transform.Find("ButtonPlay").GetComponent<Button>();
+        Button buttonHowToPlay = transform.Find("ButtonHowToPlay").GetComponent<Button>();
+        buttonPlay.onClick.AddListener(OnPlayClick);
+        buttonHowToPlay.onClick.AddListener(OnHowToPlayClick);
+    }
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            ShowExitDialog();
+        }
+    }
+
+    void OnPlayClick(){
+        Debug.Log("Play Clicked");
+        StartGameScene();        
+    }
+
+    void OnHowToPlayClick(){
+        Debug.Log("How To Play Clicked");
+    }
+
+    void ShowExitDialog(){
+        Debug.Log("Exit Clicked");
+    }
+
+    void StartGameScene(){
+        SceneManager.LoadScene("GameScene");
+    }
+}
